e5b2395115d097a7501294d902b82cbf1b1afa42
phanikolluru <106084523+phanikolluru@users.noreply.github.com>
2022-08-31 00:14:02 +0530

    Add files via upload

---
+++ GitTest1.java
@@ -0,0 +1,19 @@
+
+public class GitTest1 
+{
+
+	public static void main(String[] args) 
+	{
+		String hungry = "yes";
+		if(hungry == "yes")
+		{
+			System.out.println("Eat something");
+		}
+		else
+		{
+			System.out.println("Do some work");
+		}
+		
+	}
+	
+}
